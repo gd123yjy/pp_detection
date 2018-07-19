e4f1c5fc162d17fced5d8dc5c96f20660950d94c
yjy <1224624112@qq.com>
2018-07-19 16:21:44 +0800

    finish HandDetector Module,pipeline to be implemented

--- src/HandDetector.cpp
+++ src/HandDetector.cpp
@@ -14,14 +14,53 @@ HandDetector::HandDetector(ImageReader ir)
 	//todo: asert ir.getImg() is binary image
 }
 
-
 HandDetector HandDetector::detect()
 {
-	//todo:eee
-	this->ir.getImg().rowRange(60,420).colRange(100,550).copyTo(this->result);
+	cv::Mat gray_img;
+	vector<vector<cv::Point>> contours;
+	int max_x, min_x, max_y, min_y, max_area, max_area_i, count;
+
+	min_x = min_y = 1e6;
+	max_x = max_y = max_area = max_area_i = count = 0;
+
+	assert(this->ir.getImg().type() == CV_8U);
+	cv::findContours(this->ir.getImg(), contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+	for (vector<vector<cv::Point>>::iterator i = contours.begin(); i != contours.end(); i++)
+	{
+		int area = cv::contourArea(*i);
+		if (area > max_area)
+		{
+			max_area = area;
+			max_area_i = count;
+		}
+		count++;
+	}
+
+	for (vector<cv::Point>::iterator i = contours[max_area_i].begin(); i != contours[max_area_i].end(); i++)
+	{
+		if (max_x < i->x)
+		{
+			max_x = i->x;
+		}
+		if (max_y < i->y)
+		{
+			max_y = i->y;
+		}
+		if (min_x > i->x)
+		{
+			min_x = i->x;
+		}
+		if (min_y > i->y)
+		{
+			min_y = i->y;
+		}
+	}
+
+	this->ir.getImg().rowRange(min_y, max_y).colRange(min_x, max_x).copyTo(this->result);
+
 	return *this;
 }
-
 
 void HandDetector::saveResult(string path)
 {
--- src/palmprint_dectection.cpp
+++ src/palmprint_dectection.cpp
@@ -6,64 +6,54 @@
 
 using namespace cv;
 
-int distance(Vec3b a,Vec3b b)
+int distance(Vec3b a, Vec3b b)
 {
-	return sqrt(pow((a[0]-b[0]),2)+pow((a[1]-b[1]),2)+pow((a[2]-b[2]),2));
+	return sqrt(pow((a[0] - b[0]), 2) + pow((a[1] - b[1]), 2) + pow((a[2] - b[2]), 2));
 }
 
 Mat binary(Mat colorImg)
 {
 	Mat result;
 
-	result.create(colorImg.size(),CV_8U);  
+	result.create(colorImg.size(), CV_8U);
 
 	//binary basing on skin color model
 	//bgr(38,55,71)~bgr(20,26,30) mid(29,40,50)
-    MatConstIterator_<Vec3b> it_in=colorImg.begin<Vec3b>();  
-    MatConstIterator_<Vec3b> itend_in=colorImg.end<Vec3b>();  
-    MatIterator_<uchar> it_out=result.begin<uchar>();  
-    MatIterator_<uchar> itend_out=result.end<uchar>();  
-    while(it_in!=itend_in)  
-    {  
-		if(distance(*it_in,Vec3b(29,40,50))<35)
+	MatConstIterator_<Vec3b> it_in = colorImg.begin<Vec3b>();
+	MatConstIterator_<Vec3b> itend_in = colorImg.end<Vec3b>();
+	MatIterator_<uchar> it_out = result.begin<uchar>();
+	MatIterator_<uchar> itend_out = result.end<uchar>();
+	while (it_in != itend_in)
+	{
+		if (distance(*it_in, Vec3b(29, 40, 50)) < 28)
 		{
-			(*it_out)=200;  
+			(*it_out) = 200;
 		}
 		else
 		{
-			(*it_out)=0; 
+			(*it_out) = 0;
 		}
 
-        it_in++;  
-        it_out++;  
-    }  
+		it_in++;
+		it_out++;
+	}
 
 	return result;
 }
 
-void matchTemplateFromBinaryImg(cv::Mat colorImg,cv::Mat binaryTemplate)
+void matchTemplateFromBinaryImg(cv::Mat colorImg, cv::Mat binaryTemplate, Point &maxLoc, Point &maxLoc_diangle)
 {
-	//const string input_path = "/media/yjy/DATA1/VSProject/palmprint_dectection/data/input";
-	const string output_path = "/media/yjy/DATA1/VSProject/palmprint_dectection/data/output/";
-	const string file_name = "IMG_0272_002_LLL_015_107";
-
-	double minVal; double maxVal; cv::Point minLoc; cv::Point maxLoc;
+	double minVal;
+	double maxVal;
+	cv::Point minLoc;
 	cv::Mat result;
 
-	cv::imshow("binary color Img", binary(colorImg));
-	cv::imshow("binary Template Img", binaryTemplate);
-	cv::waitKey();
-
-	cv::matchTemplate(binary(colorImg), binaryTemplate, result,cv::TM_CCOEFF);//TM_SQDIFF
+	cv::matchTemplate(binary(colorImg), binaryTemplate, result, cv::TM_CCOEFF); //TM_SQDIFF
 
 	cv::minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, cv::Mat());
-	cv::rectangle(colorImg, maxLoc, cv::Point(maxLoc.x + binaryTemplate.cols, maxLoc.y + binaryTemplate.rows), cv::Scalar(0,155,0), 2, 8, 0);
 
-	cv::imwrite(output_path + file_name + "-output.png", colorImg);
-	cv::imshow("result", colorImg);
-	cv::waitKey(0);
+	maxLoc_diangle = Point2d(maxLoc.x + binaryTemplate.cols, maxLoc.y + binaryTemplate.rows);
 }
-
 
 int main()
 {
@@ -71,14 +61,23 @@ int main()
 	const string img_save_base = "/media/yjy/DATA1/VSProject/palmprint_dectection/data/output/";
 	const string file_name = "IMG_0272_002_LLL_015_107";
 
+	cv::Point maxLoc, maxLoc_diangle;
 	cv::Mat colorImg = cv::imread(img_load_base + file_name + "-color.bmp");
 
-	ImageReader ir(img_load_base + file_name +"-gray.bmp");
+	ImageReader ir(img_load_base + file_name + "-gray.bmp");
 	//ir.loadImg();
 	HandDetector hd(ir);
 
-	matchTemplateFromBinaryImg(colorImg,hd.detect().getBinaryTemplate());
+	Mat binaryTemplate = hd.detect().getBinaryTemplate();
+	matchTemplateFromBinaryImg(colorImg, binaryTemplate, maxLoc, maxLoc_diangle);
+	cv::rectangle(colorImg, maxLoc, maxLoc_diangle, cv::Scalar(0, 155, 0), 2, 8, 0);
 
-    return 0;
+	cv::imwrite(img_save_base + file_name + "-output.png", colorImg);
+
+	cv::imshow("binary color Img", binary(colorImg));
+	cv::imshow("binary Template Img", binaryTemplate);
+	cv::imshow("result", colorImg);
+	cv::waitKey(0);
+
+	return 0;
 }
-
